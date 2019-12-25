9379d6e36f316395a67b04d914c10f683daeeb78
“James” <kangjs@17biyi.com>
2019-12-25 15:49:47 +0800

    ceshi

--- app/src/main/java/com/example/gittest/MainActivity.java
+++ app/src/main/java/com/example/gittest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gittest;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("hahha","ceshi======");
+        Log.d("hahha","222======");
+        Log.d("hahha","222======");
+        Log.d("hahha","222======");
+        Log.d("hahha","222======");
     }
 }
